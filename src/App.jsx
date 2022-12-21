bc7b2a69a920f694b42bc1f5f8426530899b1f34
Kev <kkauf355@gmail.com>
2022-12-20 21:02:28 -0500

    5 questions dispaly. question is linked properly. need to link answers, check answers, and style

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,41 @@ import Question from './Components/Question'
 function App() {
   const [quizState, setQuizState] = React.useState(0) //0 no quiz, 1 quiz running, 2 show answers
   const [answers, setAnswers] = React.useState([])
-  const [answerKey, setAnswerKey] = React.useState([])
+  const [questions, setQuestions] = React.useState([])
+
+  const questionElements = questions.map((q, i) => {
+    console.log(`q = ${q} :: what is q's type? ${typeof q}`)
+    return (
+      <Question 
+        key={i}
+        question={q.question}
+        correctAnswer={q.correct_answer}
+        incorrectAnswer={q.incorrect_answer}
+      />
+    )
+  })
 
   React.useEffect(() => {
     async function getQuestions() {
-        const res = await fetch("https://opentdb.com/api.php?amount=10")
+        const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
         const data = await res.json()
-        const apiResponse = JSON.stringify(data.response_code)
-        const stringData = JSON.stringify(data.results)
-        //setAllMemes(data.data.memes)
+        const responseCode = JSON.stringify(data.response_code)
+        const dataResults = data.results
+        setQuestions(dataResults)
 
-        console.log(`getQuestions: data.response_code = ${apiResponse}, data.results = ${stringData}`)
+        //console.log(`getQuestions: type of data.results = ${typeof dataResults}, data.results = ${dataResults}`)
     }
     getQuestions()
-}, [])
+  }, [])
 
-function checkAnswers() {
+  function checkAnswers() {
 
-}
+  }
 
-function startQuiz() {
-  console.log("startQuiz")
-  setQuizState(1)
-}
+  function startQuiz() {
+    console.log("startQuiz")
+    setQuizState(1)
+  }
 
   return (
     <div className="App">
@@ -34,7 +46,7 @@ function startQuiz() {
         <button onClick={startQuiz}>Start quiz</button>
       </div>
       {quizState !== 0 && <div className='quiz-screen'>
-        <Question />
+        {questionElements}
         <button onClick={checkAnswers}>Check Answers</button>
       </div>}
     </div>
--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 
-function Question() {
+function Question(props) {
+    const allAnswers = props.incorrectAnswer.join(props.correctAnswer)
 
     function handleChange(event) {
         console.log("handleRadioChange")
     }
 
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+
     return (
         <section>
-            <h2>Question 1</h2>
+            <h2>{props.question}</h2>
             <div className='answer-section'>
                 <label>
                     <input 
